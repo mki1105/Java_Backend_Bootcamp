af0e6028b5e1c35631927a279860dacd468025aa
mki1105 <mki1105@naver.com>
2022-06-01 15:13:53 +0900

    examservlet

---
+++ Servlet_0601/Webapp/views/result.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>결과</title>
+</head>
+<body>
+
+${operation} 처리결과는 ${result} 입니다.
+<!-- operation : 덧셈 뺼셈 곱셈 나눗셈 
+ 	result에는 해당 연산을 실행한 결과 값 -->
+</body>
+</html>
---
+++ Servlet_0601/examservlet/MinCalculator.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MinCalculator implements Calculator {
+
+	@Override
+	public String calculate(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		int n1 = Integer.parseInt(req.getParameter("n1"));
+		int n2 = Integer.parseInt(req.getParameter("n2"));		
+		int result = n1 - n2;
+		String operation = "뺴기";
+		req.setAttribute("operation", operation);
+		req.setAttribute("result", result);	
+		return "result";
+	}
+}
